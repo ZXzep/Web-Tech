34b3d57d259d272dd8c13e3bf5c6c6bbf9cd3c70
gomesaoo21 <thanabordee@it.kmitl.ac.th>
2022-10-12 12:24:14 +0700

    edits

--- lab8/3/index.php
+++ lab8/3/index.php
@@ -12,84 +12,94 @@
 </head>
 
 <style>
-    * {
-        margin: 0;
-        padding: 0;
-        font-family: 'Kanit', sans-serif;
-    }
+* {
+    margin: 0;
+    padding: 0;
+    font-family: 'Kanit', sans-serif;
+}
 
-    body {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        background-color: #202020;
-        height: 100vh;
-    }
+body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #202020;
+    height: 100vh;
+}
 
-    .card {
-        background: #eee;
-        border-radius: 10px;
-        height: 90vh;
-        width: 60vh;
-    }
+.card {
+    background: #eee;
+    border-radius: 10px;
+    height: 90vh;
+    width: 60vh;
+}
 
-    .card div {
-        display: flex;
-        justify-content: center;
-    }
+.card div {
+    display: flex;
+    justify-content: center;
+}
 
-    img {
-        margin-top: 10%;
-        width: 70%;
-        border-radius: 10px;
-        border: 3px solid black;
-    }
-    h2{
-        margin: 4% 0;
-    }
-    .underline{
-        border-bottom: 2px solid #585858;
-        width: 90%;
-        margin-left: 5%;
-    }
-    .instruction{
-        margin: 4% 6%;
-    }
-    p{
-        text-align: center;
-        display: -webkit-box;
-        -webkit-line-clamp: 4;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        -webkit-box-orient: vertical;   
-    }
-    .bottom{
-        margin-top: 2%;
-    }
-    button{
-        padding: 0 1%;
-        border: 0px;
-        background-color: #323232;
-        color: #eee;
-        border-radius: 10px;
-        transition: 0.2s linear;
-    }
-    button:hover{
-        cursor: pointer;
-        background-color: #464646;
-    }
-    #refresh:hover{
-        cursor: pointer;
-        opacity: 60%;
-    }
-    #refresh{
-        width: 10%;
-        transition: 0.2s linear;
-    }
-    a{
-        display: flex;
-        text-decoration: none;
-    }
+img {
+    margin-top: 10%;
+    width: 70%;
+    border-radius: 10px;
+    border: 3px solid black;
+}
+
+h2 {
+    margin: 4% 0;
+}
+
+.underline {
+    border-bottom: 2px solid #585858;
+    width: 90%;
+    margin-left: 5%;
+}
+
+.instruction {
+    margin: 4% 6%;
+}
+
+p {
+    text-align: center;
+    display: -webkit-box;
+    -webkit-line-clamp: 4;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    -webkit-box-orient: vertical;
+}
+
+.bottom {
+    margin-top: 2%;
+}
+
+button {
+    padding: 0 1%;
+    border: 0px;
+    background-color: #323232;
+    color: #eee;
+    border-radius: 10px;
+    transition: 0.2s linear;
+}
+
+button:hover {
+    cursor: pointer;
+    background-color: #464646;
+}
+
+#refresh:hover {
+    cursor: pointer;
+    opacity: 60%;
+}
+
+#refresh {
+    width: 10%;
+    transition: 0.2s linear;
+}
+
+a {
+    display: flex;
+    text-decoration: none;
+}
 </style>
 
 <body>
@@ -123,7 +133,7 @@
             </a>
         </div>
     </div>";
-        ?>
-        </body>
-        
+            ?>
+</body>
+
 </html>
