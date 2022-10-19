83bd18d68a6c355d6af104c0eeac51bfc7c2d4bd
Xzep <zephyrxzep@gmail.com>
2022-10-19 21:00:58 +0700

    edits

--- lab9/1/index.php
+++ lab9/1/index.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -74,42 +74,3 @@
         font-family: 'Kanit', sans-serif;
     }
 </style>
-
-=======
-<html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Document</title>
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-</head>
-<body>
-<?php
-
-class MyDB extends SQLite3 {
-   function __construct() {
-      $this->open('customers.db');
-   }
-}
-$db = new MyDB();
-$sql ="SELECT * from CUSTOMERS";
-$ret = $db->query($sql);
-echo '<table class="table"><thead><tr><th scope="col">ID</th><th scope="col">Name</th><th scope="col">Address</th><th scope="col">Phone</th><th scope="col">Email</th></tr></thead><tbody><tr>';
-while($row = $ret->fetchArray(SQLITE3_ASSOC) ) {
-   echo '<tr><td>'.$row['CustomerId'].'</td><td>'.$row['FirstName'].' '.$row['LastName'].'</td><td>'.$row['Address'].' '.$row['City'].' '.$row['State'].$row['Country'].' '.$row['PostalCode'].'</td>'.'<td>'.$row['Phone'].' '.$row['Fax'].'</td>'.'<td>'.$row['Email'].'</td>';
-}
-echo '</table>';
-if ($_GET) {
-   $em_id = $db->querySingle("SELECT * FROM CUSTOMERS ORDER BY CustomerId DESC LIMIT 1;");
-   $sql = "DELETE from CUSTOMERS where CustomerId = $em_id;";
-   $ret = $db->exec($sql);
-}
-$db->close();
-?>
-<form action="index.php" method="get">
-<input type="submit" name="insert" value="Delete last row"/>
-</form>
-</body>
->>>>>>> 34b3d57d259d272dd8c13e3bf5c6c6bbf9cd3c70
-</html>
